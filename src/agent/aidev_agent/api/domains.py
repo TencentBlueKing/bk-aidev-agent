4c142484338c7ae6fa5319dd20d12619c945665f
jasmineqin <jasmineqin@canway.net>
2025-09-26 19:54:10 +0800

    fix: ssm_client删除多余的代码实现，只保留 api 转发配置 --story=235

--- src/agent/aidev_agent/api/domains.py
+++ src/agent/aidev_agent/api/domains.py
@@ -7,5 +7,5 @@ from aidev_agent.config import settings
 BKAIDEV_URL = settings.BK_AIDEV_APIGW_ENDPOINT or get_endpoint(settings.BK_AIDEV_GATEWAY_NAME, settings.BK_APIGW_STAGE)
 
 
-# SSM服务相关配置
+# SSM服务相关配置（默认值仅供本地开发使用，请在生产环境中配置正确的地址）
 SSM_URL = settings.BK_SSM_ENDPOINT
