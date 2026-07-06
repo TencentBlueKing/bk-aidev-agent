8bb1104e564b1f44976079710b47ce1718dcdab4
xuzhan <xuzhan@tencent.com>
2026-07-06 10:41:09 +0800

    --story=133935145 人机协同

--- src/frontend/ai-blueking/packages/chat-helper/src/agent/use-agent.ts
+++ src/frontend/ai-blueking/packages/chat-helper/src/agent/use-agent.ts
@@ -265,7 +265,7 @@ export const useAgent = (mediator: IMediatorModule, protocol: ISSEProtocol) => {
               // 如果会话不匹配，则不继续轮询
             if (sessionCode !== mediator.session?.current?.value?.sessionCode) return;
             pollResumeSession(sessionCode);
-          }, 15000);
+          }, 10000);
         }
       });
     }
