f50fa07b398ea2c28b489c1c8806a3f7e9fd7948
bennychen <bennychen@tencent.com>
2026-07-31 20:59:31 +0800

    minor: 统一版本至 2.2.0post3 --story=136206810

--- src/plugins/aidev_wxbot/aidev_wxbot/__init__.py
+++ src/plugins/aidev_wxbot/aidev_wxbot/__init__.py
@@ -8,5 +8,5 @@ A Django app for WeChat AI bot integration with BK AI Dev platform.
 
 default_app_config = "aidev_wxbot.apps.AidevWxbotConfig"
 
-__version__ = "2.2.0post2"
+__version__ = "2.2.0post3"
 __author__ = "tencentblueking"
