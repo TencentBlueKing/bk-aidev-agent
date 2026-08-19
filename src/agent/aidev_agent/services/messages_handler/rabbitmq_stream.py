09dcacb5c715b86a13977407f1410e1585100407
KailongJiang <xiaoj655@gmail.com>
2026-08-19 15:11:02 +0800

    chore

--- src/agent/aidev_agent/services/messages_handler/rabbitmq_stream.py
+++ src/agent/aidev_agent/services/messages_handler/rabbitmq_stream.py
@@ -358,7 +358,7 @@ class RabbitMQStreamMessageHandler(RabbitMQMessageHandler):
             "vhost": env.str("RABBITMQ_VHOST", "/"),
             "heartbeat": env.int("RABBITMQ_STREAM_HEARTBEAT", 60),
             "max_retries": env.int("RABBITMQ_STREAM_MAX_RETRIES", 20),
-            "load_balancer_mode": True
+            "load_balancer_mode": env.bool("RABBITMQ_STREAM_LB_MODE", True)
         }
 
     def _get_stream_name(self, thread_id: str) -> str:
